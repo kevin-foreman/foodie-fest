ce0a10261d5af35d58fa2281baf6a2869ebab45d
kevin-foreman <kevin.o.foreman2@gmail.com>
2022-06-13 13:45:31 -0500

    Code base downloaded

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,17 @@
+const webpack = require("webpack")
 const path = require("path");
 
-const webpack = require("webpack");
-
-module.exports = { 
-    entry: './assets/js/script.js',
-
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-            filename: 'main.bundle.js'
-    },
-    plugins: [
-        new webpack.ProvidePlugin({
-            $: "jquery",
-            jQuery: "jquery"
-        }),
-    ],
-    mode: 'development'
-
+module.exports = {
+  entry: "./assets/js/script.js",
+  output: {
+    path: path.join(__dirname + "/dist"),
+    filename: "main.bundle.js"
+  },
+  plugins: [
+  new webpack.ProvidePlugin({
+    $: "jquery",
+    jQuery: "jquery"
+  }),
+  ],
+  mode: "development"
 };
